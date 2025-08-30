b11c3ab7656c3a2375125272ca75ca2032b3cd79
Nithin1255 <kuppamnithin786@gmail.com>
2025-08-30 16:09:20 +0530

    remove localhost

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
     origin: [
-        'http://localhost:5173',         // keep for local development
+        // keep for local development
         'https://your-frontend.vercel.app' // add your deployed frontend
     ],
     credentials: true,
