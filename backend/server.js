d730c2be4c0f1fcdf81db10d3ac5b2e1030ed7f2
Nithin1255 <kuppamnithin786@gmail.com>
2025-08-30 15:52:31 +0530

    localhost urlupdated in backend

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://photo-grapher-online-beige.vercel.app/',
     credentials: true,
     allowedHeaders: ['Authorization', 'Content-Type'],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
